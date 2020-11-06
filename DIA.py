426f71a6a8f000027ca1cec7de8b54653d7b329b
rithvikananth <63649048+rithvikananth@users.noreply.github.com>
2020-11-06 18:15:44 -0500

    Update DIA.py

--- DIA.py
+++ DIA.py
@@ -1,15 +1,16 @@
 import random
-from pprint import pprint
+import copy
+import sys
 import time
 import math
+from pprint import pprint
+
 import numpy as np
 import pandas as pd
 import itertools
 from Environment import Cell, Environment
 from Graphics_grid import GraphicGrid
 
-# this agent is similar to the CSP  agent but additionally, it chooses an agent whose probability is the minimum when
-# it cannot infer further from the knowledge base equations.
 
 class DI_Agent:
 
@@ -24,11 +25,12 @@ class DI_Agent:
         self.knowledge_base = list()
         self.unexplored_cells = list()
 
-
     def play(self):
         self.populate_unexplored_cells()
         random_cell = random.choice(self.unexplored_cells)
         self.env.query_cell(random_cell)
+        if random_cell.is_mine:
+            random_cell.is_flagged = True
         self.unexplored_cells.remove(random_cell)
         self.render_basic_view()
         self.create_condition(random_cell)
@@ -37,7 +39,7 @@ class DI_Agent:
                 break
         print(self.mines_exploded)
 
-    def remove_dups(self,list):
+    def remove_dups(self, list):
         res = []
         for i in list:
             if i not in res:
@@ -50,7 +52,7 @@ class DI_Agent:
         for row in range(self.grid_size):
             for column in range(self.grid_size):
                 cell = self.currGrid[row][column]
-                #self.populate_cell(cell)
+                self.populate_cell(cell)
                 if (cell.curr_value is not None) and not cell.is_flagged:
                     if cell.curr_value - cell.mines_surrounding == cell.covered_neighbours:
                         if cell.curr_value != 0 and cell.covered_neighbours != 0:
@@ -59,7 +61,9 @@ class DI_Agent:
                     elif (cell.total_neighbours - cell.curr_value) - cell.safe_cells_surr == cell.covered_neighbours:
                         self.mark_neighbours_safe(cell)
                         return 'Done'
-                    self.create_condition(cell)   # here we also update the probabilities of unexplored cells
+                    self.check_for_valid_sols()
+                    self.knowledge_base = self.remove_dups(self.knowledge_base)
+                    self.create_condition(cell)
         if not self.open_random_cell():
             return 'Finished'
         return 'Done looping'
@@ -76,74 +80,103 @@ class DI_Agent:
                 if (row + i >= 0 and col + j >= 0 and row + i < self.env.n and col + j < self.env.n):
                     cell1 = self.currGrid[row + i][col + j]
                     self.populate_cell(cell1)
-                    if cell1.curr_value is not None:
-                        continue
+                    if cell1.curr_value is None and not cell1.is_flagged:
+                        condition.append(cell1)
                     if cell1.is_flagged or cell1.is_mine:
                         constraint_value -= 1
                         continue
-                    else:
-                        condition.append(cell1)
-        if len(condition) == constraint_value:
+                    if cell1.curr_value is not None:
+                        continue
+        if len(condition) == constraint_value and not constraint_value < 0:
             for cell in condition:
                 cell.is_flagged = True
                 if cell in self.unexplored_cells:
                     self.unexplored_cells.remove(cell)
-        elif condition and condition not in self.knowledge_base:
-            self.knowledge_base.append([condition,constraint_value])
-            # we find the probability of the unexplored cells which are in the knowledge base
-            # whenever we update our knowledge base
-            self.probability()
+        elif condition and condition not in [con[0] for con in self.knowledge_base] and constraint_value >= 0:
+            self.knowledge_base.append([condition, constraint_value])
 
+    def substitute_values(self, kb):
+        for index, equation in enumerate(kb):
+            cells_part = equation[0]
+            for cell in cells_part:
+                if cell.curr_value is not None and not cell.is_flagged and not cell.is_mine:
+                    cells_part.remove(cell)
+                elif cell.is_flagged or cell.is_mine:
+                    cells_part.remove(cell)
+                    equation[1] = equation[1] - 1
+            if len(cells_part) == 0:
+                kb.remove(equation)
 
+    def check_for_valid_sols(self):
+        for index, equation in enumerate(self.knowledge_base):
+            cells_part = equation[0]
+            if len(cells_part) == 1:
+                if equation[1] == 0:
+                    if cells_part[0].curr_value is not None:
+                        self.safe_cells.append(cells_part[0])
+                elif equation[1] == 1:
+                    cells_part[0].is_flagged = True
+                self.knowledge_base.remove(equation)
+            elif len(cells_part) == equation[1] and not equation[1] < 0:
+                for cell in cells_part:
+                    cell.is_flagged = True
+                self.knowledge_base.remove(equation)
+        self.substitute_values(self.knowledge_base)
 
-    def possible_solutions(self,knowledge_base):
-        unique_variables = []
-        for condition in knowledge_base:
+    def possible_solutions(self):
+        self.substitute_values(self.knowledge_base)
+        unique_variables = {}
+        self.knowledge_base = self.remove_dups(self.knowledge_base)
+        self.check_for_valid_sols()
+        for condition in self.knowledge_base:
             for variable in condition[0]:
-                if variable not in unique_variables:
-                    unique_variables.append(variable)
-        max_variables = 30 if len(unique_variables) > 30 else len(unique_variables)
-        probable_sol = []
-        max_variables_list = random.choices(unique_variables,k = max_variables)
-        lst = list(map(list, itertools.product([0, 1], repeat=len(max_variables_list))))
-        for assignment in lst:
-            flag = 0
-            sat = 0
-            for condition in knowledge_base:
-                sum = condition[1]
-                sol = 0
-                j = 0
-                f = 0
-                for var in condition[0]:
-                    j += 1
-                    if var in max_variables_list:
-                        i = max_variables_list.index(var)
-                        sol += assignment[i]
-                    else:
-                        break;
+                if variable not in unique_variables.keys():
+                    unique_variables[variable] = 1
                 else:
-                    if j == len(condition[0]):
-                        sat += 1
-                        if (sol != sum):
-                            break;
-                        if (sol == sum):
-                            flag += 1
-            if flag == sat:
-                probable_sol.append(assignment)
-        probable_sol_df = pd.DataFrame(probable_sol)
-        domain = []
-        for col in probable_sol_df:
-            domain.append(probable_sol_df[col].unique().tolist())
-        var_domain = dict(zip(max_variables_list, domain))
-        for var in var_domain:
-            if var_domain[var] == [1]:
-                self.mine_cells.append(var)
-                if var in self.unexplored_cells:
-                    self.unexplored_cells.remove(var)
-                var.is_flagged = True
-            elif var_domain[var] == [0]:
-                self.safe_cells.append(var)
-        for condition in knowledge_base:
+                    unique_variables[variable] += 1
+        interesting_vars = []
+        for key in unique_variables.keys():
+            if unique_variables[key] > 1:
+                interesting_vars.append(key)
+        for var in interesting_vars:
+            fake_vals = [0, 1]
+            if var.is_flagged:
+                continue
+            for fake_val in fake_vals:
+                if var.is_flagged:
+                    continue
+                dup_kb = copy.deepcopy(self.knowledge_base)
+                prev_val = var.curr_value
+                if fake_val == 0:
+                    var.curr_value = fake_val
+                else:
+                    var.is_flagged = True
+                self.substitute_val_in_kb(var, dup_kb)
+                # Check if Kb is breaking
+                var.curr_value = prev_val
+                if fake_val == 1 and var.is_flagged:
+                    var.is_flagged = False
+                if not self.solve_dup_kb(dup_kb):
+                    # if not self.validate_kb(dup_kb):
+                    if fake_val == 0:
+                        var.is_flagged = True
+                        self.mine_cells.append(var)
+                        if var in self.unexplored_cells:
+                            self.unexplored_cells.remove(var)
+                        if self.env.grid[var.row][var.col] != -1:
+                            print("wrongly predicted")
+                            sys.exit()
+                    else:
+                        if var not in self.safe_cells:
+                            self.safe_cells.append(var)
+                        # self.env.query_cell(var)
+                        if self.env.grid[var.row][var.col] == -1:
+                            print("wrongly predicted")
+                            sys.exit()
+                    self.check_for_valid_sols()
+                    break
+                self.check_for_valid_sols()
+        for condition in self.knowledge_base:
             for safe_cell in self.safe_cells:
                 if safe_cell in condition[0]:
                     condition[0].remove(safe_cell)
@@ -152,6 +185,43 @@ class DI_Agent:
                     condition[0].remove(mine_cell)
                     condition[1] -= 1
 
+    def is_kb_solvable(self, kb):
+        for index, equation in enumerate(kb):
+            cells_part = equation[0]
+            if len(cells_part) == equation[1]:
+                return True
+            if len(cells_part) == 1:
+                return True
+        return False
+
+    def solve_dup_kb(self, kb):
+        while self.is_kb_solvable(kb):
+            for index, equation in enumerate(kb):
+                cells_part = equation[0]
+                if equation[1] < 0 or len(cells_part) < equation[1]:
+                    return False
+                if len(cells_part) == equation[1]:
+                    # flag all cells
+                    for cell in cells_part:
+                        cell.is_flagged = True
+                if len(cells_part) == 1:
+                    if equation[1] == 0:
+                        cells_part[0].curr_value = 0
+                    elif equation[1] == 1:
+                        cells_part[0].is_flagged = True
+            self.substitute_values(kb)
+        return True
+
+    def substitute_val_in_kb(self, change_cell, kb):
+        for equation in kb:
+            cells_part = equation[0]
+            for cell in cells_part:
+                if change_cell.row == cell.row and change_cell.col == cell.col:
+                    if change_cell.curr_value is not None and not change_cell.is_flagged and not change_cell.is_mine:
+                        cells_part.remove(cell)
+                    elif change_cell.is_flagged or change_cell.is_mine:
+                        cells_part.remove(cell)
+                        equation[1] = equation[1] - 1
 
     def populate_unexplored_cells(self):
         for row in range(self.grid_size):
@@ -181,7 +251,7 @@ class DI_Agent:
                 total_neighbours += 1
                 if neighbour.curr_value is None and not neighbour.is_mine and not neighbour.is_flagged:
                     covered += 1
-                elif neighbour.is_flagged:
+                elif neighbour.is_flagged or neighbour.is_mine:
                     mines += 1
                 else:
                     safe += 1
@@ -200,6 +270,9 @@ class DI_Agent:
                     if neighbour in self.unexplored_cells:
                         self.unexplored_cells.remove(neighbour)
                     self.env.query_cell(neighbour)
+                    if neighbour.is_mine:
+                        print('Queried wrongly')
+                        sys.exit()
         self.render_basic_view()
 
     def flag_neighbours(self, cell):
@@ -234,28 +307,26 @@ class DI_Agent:
         if not self.have_free_cells():
             return False
         random_cell = self.get_safe_cells()
-
         if not random_cell:
-            self.possible_solutions(self.remove_dups(self.knowledge_base))
+            self.possible_solutions()
             random_cell = self.get_safe_cells()
             self.render_basic_view()
-            # we choose a random cell with probability
             if not random_cell:
                 prob = 2
-                # self.probability()
-                # prob1 = []
+                self.probability()
                 for cell in self.unexplored_cells:
                     if cell.probability != None:
                         min_cell = cell
                         if min_cell.probability < prob:
                             prob = min_cell.probability
-                            # prob1.append(prob)
                             random_cell = min_cell
                     else:
                         continue
-                # print(prob1)
             if not random_cell:
-                    random_cell = random.choice(self.unexplored_cells)
+                random_cell = random.choice(self.unexplored_cells)
+                while random_cell.is_flagged or (random_cell.curr_value is not None):
+                    random_cell = self.currGrid[random.randrange(0, len(self.currGrid))][
+                        random.randrange(0, len(self.currGrid))]
 
         if random_cell in self.unexplored_cells:
             self.unexplored_cells.remove(random_cell)
@@ -277,7 +348,6 @@ class DI_Agent:
                     numeric_grid[row][column] = 'f'
                 if self.currGrid[row][column].is_mine:
                     numeric_grid[row][column] = 'b'
-        # pprint(numeric_grid)
 
     # IF cell == 1 finding count value
     # Substitute cell value as 1 and check for the number of valid possibilities
@@ -353,7 +423,11 @@ class DI_Agent:
         else:    # we find all possible combinations
             a = 1
             for i in equation_list:
-                a *= math.factorial(len(i[0])) / (math.factorial(i[1]) * math.factorial(len(i[0]) - i[1]))  # nCr formula
+                den = len(i[0]) - i[1]
+                if den < 0 or len(i[0]) < 0 or i[1] < 0:
+                    continue
+                else:
+                    a *= math.factorial(len(i[0])) / (math.factorial(i[1]) * math.factorial(den))  # nCr formula
             return a
 
 # Substitute cell value as 0 and check for the number of valid possibilities
@@ -428,42 +502,58 @@ class DI_Agent:
         else:               # we find all possible combinations
             a = 1
             for i in equation_list:
-                a *= math.factorial(len(i[0])) / (math.factorial(i[1]) * math.factorial(len(i[0]) - i[1]))  # nCr formula
+                den = len(i[0]) - i[1]
+                if den < 0 or len(i[0]) < 0 or i[1] < 0:
+                    continue
+                else:
+                    a *= math.factorial(len(i[0])) / (math.factorial(i[1]) * math.factorial(den)) # nCr formula
             return a
 
     # probability of each cell is the count of cell being a mine divided by total possibilities (is mine and not a mine)
     def probability(self):
-        conditions = [condition[0] for condition in self.knowledge_base]
+        self.possible_solutions()
+        conditions = []
+        for condition in self.knowledge_base:
+            for variable in condition[0]:
+                if variable not in conditions:
+                    conditions.append(variable)
         for cell in self.unexplored_cells:
             if cell in conditions:
                 cell.probability = self.sub_1(cell, self.knowledge_base) / (self.sub_1(cell, self.knowledge_base) + self.sub_0(cell, self.knowledge_base))
 
 
-
-# env = Environment(10, 0.3)
+# env = Environment(10, 0.4)
 # agent = DI_Agent(env)
 # agent.play()
-
+# Driver code to test
 
 # Driver code to test
 density_store = {}
+flag_store = {}
 # Iterating for range of mine density
 for d in range(1, 10, 1):
-    density = d/10
-    Store = {'bombs': [], 'time': []}
-    for i in range(5):
+    density = d / 10
+    Store = {'bombs': [], 'time': [], 'flagged': []}
+    for i in range(10):
         start = time.process_time()
-        env = Environment(10, density)
+        env = Environment(20, density)
+        mines = env.m
         agent = DI_Agent(env)
         agent.play()
         Store['bombs'].append(agent.mines_exploded)
+        Store['flagged'].append((mines - agent.mines_exploded) / mines)
         Store['time'].append(time.process_time() - start)
 
     print('Average number of bombs exploded is ' + str(np.average(Store['bombs'])))
     print('Average time taken ' + str(np.average(Store['time'])))
+    print('Average flags ' + str(np.average(Store['flagged'])))
     density_store[density] = str(np.average(Store['bombs']))
+    flag_store[density] = str(np.average(Store['flagged']))
 print(density_store)
 for key in density_store.keys():
     print(str(key) + ',' + str(density_store[key]))
+for key in flag_store.keys():
+    print(str(key) + ',' + str(flag_store[key]))
+
 
 
